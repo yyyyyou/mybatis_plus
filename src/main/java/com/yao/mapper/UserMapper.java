ce61bec852fb5bc0a9686097660b3b95761c9704
ydana <1092528846@qq.com>
2021-12-15 11:02:02 +0800

    修改市区问题

--- src/main/java/com/yao/mapper/UserMapper.java
+++ src/main/java/com/yao/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.yao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yao.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
